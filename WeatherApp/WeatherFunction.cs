b0c74a0c29878402aa9bbf1a7e6972d41039c0b8
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2022-07-21 18:15:10 +1200

    Initial working commit of code

---
+++ WeatherApp/WeatherFunction.cs
@@ -0,0 +1,48 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace WeatherApp;
+
+public class WeatherFunction
+{
+    private readonly ILogger _logger;
+    private readonly string[] WeatherEffects = new[]
+    {
+        "Clear skies", "Harsh sunlight", "Extremely harsh sunlight", "Rain",
+        "Heavy rain", "Sandstorm", "Balmy", "Hail", "Fog", "Strong winds"
+    };
+
+    private readonly string[] Regions = new[]
+    {
+        "Kanto", "Johto", "Hoenn", "Unova", "Kalos", "Alola", "Galar"
+    };
+
+    public WeatherFunction(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<WeatherFunction>();
+    }
+
+    [Function("WeatherFunction")]
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        var response = req.CreateResponse(HttpStatusCode.OK);        
+
+        var forecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            Region = Regions[Random.Shared.Next(Regions.Length)],
+            Outlook = WeatherEffects[Random.Shared.Next(WeatherEffects.Length)]
+        });
+
+        response.WriteString("World weather:");
+        await response.WriteAsJsonAsync(forecasts);
+
+        return response;
+    }
+
+    private readonly record struct WeatherForecast(DateTime Date, string Region, string Outlook);
+}
